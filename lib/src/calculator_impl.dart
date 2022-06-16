bb2c6582a068aad200a95b1ac47f1b93082754d9
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2022-06-16 18:01:47 -0300

    feat(calculator): add initial expression hierarchy

--- lib/src/calculator_impl.dart
+++ lib/src/calculator_impl.dart
@@ -9,10 +9,14 @@ abstract class Calculator {
 class _CalculatorImpl extends Calculator {
   _CalculatorImpl() : super._();
 
-  static final alphabet = '+-/*0123456789.'.codeUnits;
+  static final alphabet = '+-/*0123456789'.codeUnits;
 
   bool isInValidInput(String expression) {
     return expression.codeUnits.any((character) => !alphabet.contains(character)) || expression.isEmpty;
+  }
+
+  _Expression? _parse(String expression) {
+    return _SumExpression(_NumericExpression(2), _NumericExpression(2));
   }
 
   @override
@@ -21,6 +25,36 @@ class _CalculatorImpl extends Calculator {
       throw ArgumentError();
     }
 
-    return 4;
+    final tree = _parse(expression);
+
+    return tree == null //
+        ? throw StateError('$expression is not a valid expression')
+        : tree.evaluate();
   }
+}
+
+abstract class _Expression<T> {
+  T evaluate();
+}
+
+abstract class _AritimeticExpression implements _Expression<int> {
+  _AritimeticExpression(this.left, this.right);
+  _Expression<int> left;
+  _Expression<int> right;
+}
+
+class _SumExpression extends _AritimeticExpression {
+  _SumExpression(super.left, super.right);
+
+  @override
+  int evaluate() => left.evaluate() + right.evaluate();
+}
+
+class _NumericExpression extends _Expression<int> {
+  _NumericExpression(this.value);
+
+  final int value;
+
+  @override
+  int evaluate() => value;
 }

c9b6f55309d385bff9af5c7523f33ae71121ced3
Bruno Giacobo Pinto <bruno.giacobo.pinto@gmail.com>
2022-06-16 13:45:45 -0300

    chore(project): add initial scaffolding for the project

---
+++ lib/src/calculator_impl.dart
@@ -0,0 +1,16 @@
+abstract class Calculator {
+  Calculator._();
+
+  int compute(String expression);
+
+  factory Calculator() => _CalculatorImpl();
+}
+
+class _CalculatorImpl extends Calculator {
+  _CalculatorImpl() : super._();
+
+  @override
+  int compute(String expression) {
+    return int.parse(expression);
+  }
+}
